ad64bfbbed84afd860c2634c6b6dd5f2e0b52928
Pooja Patel <studykhuswaha01@gmail.com>
2025-08-28 22:21:27 +0530

    Added Table class

---
+++ src/Table.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/*
+print table in any number.
+ */
+public class Table
+{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter starting number in table : ");
+        int firstNumber= sc.nextInt();
+
+        int middleNumber=1;
+
+        System.out.print("enter ending number of table :");
+        int endNumber=sc.nextInt();
+
+
+        while(middleNumber <= endNumber)
+        {
+            int result = firstNumber * middleNumber;
+            System.out.println(firstNumber + " x " + middleNumber + " = " + result);
+            middleNumber++;
+        }
+    }
+}
+
